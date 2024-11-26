07ae42bddb3175137fcf56380172cb003330c136
Jesús Hernández <jesushdez1303@gmail.com>
2024-11-26 12:25:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import flet as ft
+import time
 
 
 def main(page: ft.Page):
-    t = ft.SafeArea(ft.Text(value="Hello, Flet!",color="red"))
-    page.control.append(t)
+    t = ft.Text(value="Hello, Flet!",color="red")
+    page.controls.append(t)
     page.update()
+    
+    for i in range(10):
+        t.value = f"counting {i}"
+        page.update()
+        time.sleep(1)
 
 
 ft.app(main)
