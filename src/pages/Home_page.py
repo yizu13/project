87697c69f6d899e635287b0907748038f5d739e1
Jesús Hernández <jesushdez1303@gmail.com>
2024-11-26 22:06:10 -0400

    Update Home_page.py

--- src/pages/Home_page.py
+++ src/pages/Home_page.py
@@ -2,14 +2,8 @@ import flet as ft
 
 
 def main_button(page):
-    def button_clicked(e):
-        b.data += 1
-        t.value = f"Button clicked {b.data} time(s)"
-        t.update()  # Ensure UI updates
-
     # Create the button and text
-    b = ft.ElevatedButton("Button with 'click' event", on_click=button_clicked, data=0)
-    t = ft.Text("Button clicked 0 time(s)")
-
+    time_set_button = ft.ElevatedButton("Configurar tiempo")
+    time_set_button.aligment = ft.alignment.center
     # Add controls to the page
-    page.add(b, t)
+    page.add(time_set_button)
