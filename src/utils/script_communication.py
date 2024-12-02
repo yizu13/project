465ff30dc39c6cd9271cb1577ed6d4ed47626b31
Jesús Hernández <jesushdez1303@gmail.com>
2024-12-02 02:55:07 -0400

    proving communications

--- src/utils/script_communication.py
+++ src/utils/script_communication.py
@@ -1,1 +1,38 @@
-import serial time
+import serial, time
+arduino = serial.Serial('COM12', 9600)
+time.sleep(2)
+message = "toma"
+arduino.write(message.encode())
+print(f"Sent: {message}")
+read = arduino.readline()
+print (read)
+arduino.close
+
+
+
+
+
+
+
+
+#Example code in arduino
+
+#const int led= 13;
+#void setup() {
+# // put your setup code here, to run once:
+#  Serial.begin(9600);
+#  pinMode(13, OUTPUT);
+
+#}
+
+#void loop() {
+#  // put your main code here, to run repeatedly:
+#  if (Serial.readString() == "toma"){
+#    Serial.println("si lo agarre pp");
+#    digitalWrite(13,HIGH);
+#    delay(3000);
+#    digitalWrite(13,LOW);
+#    }
+
+#}
+
