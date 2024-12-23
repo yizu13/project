b2984e46c151058fbf6fdb2e0aad43c672e7c989
Jesús Hernández <jesushdez1303@gmail.com>
2024-12-23 13:19:57 -0400

    a

--- src/pages/Home_page.py
+++ src/pages/Home_page.py
@@ -20,6 +20,7 @@ class page_home_class:
         self.text_weight_patient = ft.Text(value=e_catalog.patient_weight,theme_style=ft.TextThemeStyle.BODY_LARGE)
         self.modify_button = ft.TextButton(text="Modificar", on_click=lambda e: e_catalog.modify_information_function(e,page),icon='MODE')
         self.finalization_button = ft.ElevatedButton(text="Finalizar Terapia",on_click=lambda e:e_catalog.finish_test(e,page),icon='SEND')
+        gpnet_logo = ft.Image(src="../assets/GPNET.png",width=100,height=50,fit=ft.ImageFit.CONTAIN,)
         # Add controls to the page
         e_catalog.set_buttons_from_home_page(self.modify_button,self.finalization_button)
         self.col = ft.Column([ft.Row(controls=some_cards(page, 3))],
@@ -29,7 +30,7 @@ class page_home_class:
                                 ft.Row([ft.Text(value= "Edad del paciente:",color="blue800",theme_style=ft.TextThemeStyle.BODY_LARGE, weight=ft.FontWeight.W_700), self.text_age_patient]),
                                 ft.Row([ft.Text(value= "Peso del paciente:",color="blue800",theme_style=ft.TextThemeStyle.BODY_LARGE, weight=ft.FontWeight.W_700), self.text_weight_patient]),
                                 ft.Row([ft.Text(value= "Doctor:",color="blue800",theme_style=ft.TextThemeStyle.BODY_LARGE, weight=ft.FontWeight.W_700), self.text_name_doctor]),
-                               ft.Row([self.modify_button,self.finalization_button], alignment= ft.MainAxisAlignment.SPACE_BETWEEN)
+                               ft.Row([self.modify_button,gpnet_logo,self.finalization_button], alignment= ft.MainAxisAlignment.SPACE_BETWEEN),
                             ],
                         alignment= ft.MainAxisAlignment.END)
         
